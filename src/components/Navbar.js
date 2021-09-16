02e4ce9bedde66d725de7074ffc4be02a07ca5d4
Le Anh Duc <ladcva@yahoo.com>
2021-09-16 12:53:05 +0700

    10th

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
+import { CgMail } from "react-icons/cg";
 // import { ImBlog } from "react-icons/im";
 // import {
 //   AiFillStar,
@@ -89,7 +89,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 href="https://blogs.soumyajit.tech/"
                 target="_blank"
@@ -97,15 +97,15 @@ function NavBar() {
               >
                 Blogs
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="mailto:me@anhducle.com"
                 target="_blank"
                 className="fork-btn-inner"
               >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <CgMail style={{ fontSize: "1.2em" }} />{" "}
                 {/* <AiFillStar style={{ fontSize: "1.1em" }} /> */}
               </Button>
             </Nav.Item>
